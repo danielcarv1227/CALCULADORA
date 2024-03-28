919c37d4a2a336006e15ca68bddc3e2fc36e8764
danielcarv1227 <danielcruz@eaportal.org>
2024-03-28 16:29:24 -0300

    quarto commit do js

--- script.js
+++ script.js
@@ -32,5 +32,21 @@ botoesGorjeta.forEach(botao => {
 })
 
 function receberPorcentagem(evento) {
+    botoesGorjeta.forEach(botao => {
+        botao.classList.remove("botao-ativo")
+        if(botao.value === evento.target.value) {
+            botao.classList.add("botao-ativo")
+        }
+    })
+
+    if(evento.target.value !== "") {
     porcentagem = parseFloat(evento.target.value) / 100
+    } else {
+        porcentagem = 0
+    }
+
+    console.log(porcentagem)
 }
+
+const gorjetaInput = document.querySelector("#outra")
+gorjetaInput.addEventListener("input", receberPorcentagem)
